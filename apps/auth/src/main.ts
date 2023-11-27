e34f5c63b695657bdef04c942d63d6909ade49ce
enzo <enzo@softwareoptima.com>
2023-11-27 08:38:10 +0800

    test

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  await app.listen(3000).then(() => {
+    Logger.log(`[Test] Express server is listening to port 3000...`)
+  })
 }
 bootstrap();
--- apps/grpc/src/main.ts
+++ apps/grpc/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { NestApplicationContextOptions } from '@nestjs/common/interfaces/nest-application-context-options.interface';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   let configurations: NestApplicationContextOptions & MicroserviceOptions = {
@@ -14,7 +15,8 @@ async function bootstrap() {
   }
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, configurations);
 
-  await app.listen();
-  console.log(`Microservice is listening...`);
+  await app.listen().then(() => {
+    Logger.log(`GRPC server is listening... `)
+  })
 }
 bootstrap();
